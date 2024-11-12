9df2266d127f40f6fb64c11c58a95feedfcc661d
KakyoinRedhead <92076162+KakyoinRedhead@users.noreply.github.com>
2024-11-13 00:39:14 +0100

    sranda s míčem xd

--- ball1.py
+++ ball1.py
@@ -1,29 +1,84 @@
-import pygame
-import math 
-import numpy as np
-import random
+import math
 
-pygame.init()
+import pygame as pygame
+from pygame import Vector2, Color, mixer
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
+width = 1080
+height = 1920
 
-screen_Width = 950
-screen_Height = 950
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+screen.fill((53, 53, 88), (0, 0, width, height))
+pygame.draw.circle(screen, (255,0,0), (width/2, height/2), width/2, 5)
+mixer.init()
+sound = mixer.Sound("boing-101318.mp3")
 
-screen = pygame.display.set_mode((screen_Width, screen_Height))
-pygame.display.set_caption("Bouncing Ball")
+class Ball:
+    def __init__(self):
+        self.position = Vector2(width/2, height/2)
+        self.color = (0,0,0)
+        self.gravity = Vector2(0,0.32)
+        self.velocity = Vector2(-7,-7)
+        self.prevPos = Vector2(self.position.x,self.position.y)
+        self.radius = 30
 
-circle_radius = 450
-circle_center = np.array((screen_Width // 2, screen_Height // 2), float)
+    def update(self):
+        self.prevPos = Vector2(self.position.x,self.position.y)
 
-ball_radius = 15
-ball_color = BLUE
-ball_pos = np.array(circle_center)
-ball_vel = np.array([random.choice([-2, 2]), random.choice([-2, 2])], float)
+        # movement
+        self.velocity += self.gravity
+        self.position += self.velocity
 
+        dirToCenter = Vector2(self.position.x - width/2,self.position.y - height/2)
+        if self.isCollide():
+            pygame.mixer.Sound.play(sound)
+            self.radius += 1
+            self.position = Vector2(self.prevPos.x,self.prevPos.y)
+            v = math.sqrt(self.velocity.x * self.velocity.x + self.velocity.y * self.velocity.y)
+            angleToCollisionPoint = math.atan2(-dirToCenter.y,dirToCenter.x)
+            oldAngle = math.atan2(-self.velocity.y,self.velocity.x)
+            newAngle = 2 * angleToCollisionPoint - oldAngle
+            self.velocity = Vector2(-v * math.cos(newAngle),v * math.sin(newAngle))
 
+    def isCollide(self):
+        if self.distance(self.position.x,self.position.y,width/2,height/2) > width/2 - self.radius:
+            return True
+        return False
 
+    def distance(self, x1, y1, x2, y2):
+        return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2) * 1.0)
+
+    def draw(self):
+        pygame.draw.circle(screen,self.color,(self.position.x,self.position.y),self.radius)
+
+ball = Ball()
+color = Color(211,12,211)
+h = color.hsla[0]
+s = color.hsla[1]
+l = color.hsla[2]
+colorDir = 1
+
+while True:
+    clock.tick(60 )
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit(0)
+
+    ball.update()
+    pygame.draw.circle(screen, (255, 0, 0), (width / 2, height / 2), width / 2, 5)
+
+    color.hsla = (h, s, l, 1)
+    h += 1 * colorDir
+    if h >= 360:
+        colorDir = -1
+    elif h <= 0:
+        colorDir = 1
+
+    pygame.draw.circle(screen,
+                       (color.r, color.g, color.b),
+                       (ball.position.x, ball.position.y),
+                       ball.radius + 2)
+
+    ball.draw()
+
+    pygame.display.flip()
